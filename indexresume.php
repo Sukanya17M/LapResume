86e3006a6956252209d26a8ebc0fa692798cd1df
Sukanya Mayotha <108781019+Sukanya17M@users.noreply.github.com>
2022-10-12 11:36:15 +0700

    Update indexresume.php

--- indexresume.php
+++ indexresume.php
@@ -1,71 +1,74 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Thitipong Cheujunthuek</title>
-<meta charset="UTF-8">
+<title>Sukanya Mayotha </title>
+<h1>RESUME</h1>
+<h2>MAHASARAKHAM UNIVERSITY,THAILAND</h2>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+html,body,h2,h3,h4,h5,h6 {font-family: "LilyUPC", sans-serif}
+html,body,h1 {font-family: "Cooper Black", sans-serif}
 </style>
 </head>
 <body class="w3-light-grey">
-
 <!-- Page Container -->
-<div class="w3-content w3-margin-top" style="max-width:1400px;">
-
-  <!-- The Grid -->
+<div class="w3-content w3-margin-top" style="max-width: 1400px; font-family: Roboto, sans-serif;">
+<!-- The Grid -->
   <div class="w3-row-padding">
   
     <!-- Left Column -->
     <div class="w3-third">
     
-      <div class="w3-black w3-text-white w3-card-4">
+      <div class="w3-white w3-text-grey w3-card-4">
         <div class="w3-display-container">
-          <img src="https://drive.google.com/file/d/1dwFzcj7pisdq6TRlwHiQhmQ0D7kLB6Sn/view?usp=sharing" style="width:100%;" alt="pictures">
+          <img src="https://mahasarakham-my.sharepoint.com/:i:/r/personal/62010914605_o365_msu_ac_th/Documents/My%20Drive/OCR%20%E0%B8%AA%E0%B8%A5%E0%B8%B4%E0%B8%9B%E0%B9%80%E0%B8%87%E0%B8%B4%E0%B8%99/S__4833283.jpg?csf=1&web=1&e=bNvpL8" style="width:100%" alt="Avatar">
           <div class="w3-display-bottomleft w3-container w3-text-black">
            
           </div>
         </div>
         <div class="w3-container">
-          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-red"></i><b>Thitipong Cheujuntheuk</b></p>
-          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-green"></i>Student</p>
-          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-yellow"></i>Maha Sarakham, Thailand</p>
-          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-purple"></i>62010914602	@msu.ac.th</p>
-          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-red"></i>0880326022</p>
+          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-blue"></i><b>Sukanya Mayotha</b></p>
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-red"></i>Student</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-purple"></i>Maha Sarakham, Thailand</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-green"></i>62010914605@msu.ac.th</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-black"></i>0973378469</p>
           <hr>
-
-          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-green"></i>Skills</b></p>
-          <p>Microsoft Excel</p>
-          <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:80%">80%</div>
-          </div>
+          
+<p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-blue"></i>Skills</b></p>
           <p>Microsoft Word</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:60%">
-              <div class="w3-center w3-text-white">60%</div>
+            <div class="w3-container w3-center w3-round-xlarge w3-red" style="width:90%">90%</div>
+          </div>
+          <p>Photoshop</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
             </div>
           </div>
-          <p>Python</p>
+          <p>UiPath</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:30%">30%</div>
+            <div class="w3-container w3-center w3-round-xlarge w3-orange" style="width:75%">78%</div>
+          </div>
+          <p>Microsoft Excel</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-pink" style="width:50%">75%</div>
           </div>
           <br>
-
-          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-blue"></i>Languages</b></p>
+    <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-blue"></i>Languages</b></p>
                      <p>Thai</p>
           <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-red" style="height:24px;width:90%">90%</div>
+            <div class="w3-round-xlarge w3-center w3-pink" style="height:24px;width:90%">90%</div>
           </div>
           <p>English</p>
           <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-red" style="height:24px;width:80%">80%</div>
+            <div class="w3-round-xlarge w3-center w3-pink" style="height:24px;width:80%">60%</div>
           </div>
-          <p>Chinese</p>
+          <p>Korean</p>
           <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-red" style="height:24px;width:20%">20%</div>
+            <div class="w3-round-xlarge w3-center w3-pink" style="height:24px;width:70%">50%</div>
           </div>
           <br>
         </div>
@@ -76,81 +79,90 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
 
     <!-- Right Column -->
     <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-orange w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Work Experience</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Student / Mahasarakham Business School, Mahasarakham University, Business Information Technology Branch (B.B.A)</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022 โครงการประกวดแผนธุรกิจ<span class="w3-tag w3-blue w3-round"></span></h6>
+        </div>
+        
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>GSB Business Case Challenge </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>เข้ารอบ 20 ทีม สุดท้าย และเป็นทีมเดียวในภาคตะวันออกเฉียงเหนื่อที่เข้ารอบ</h6>       
+        </div>
+        
+          
+      </div>
+        
+               
       <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-gray w3-padding-16"><i class="fa fa-arrow-down fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Education</h2>
+        <h2 class="w3-text-orange w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Education</h2>
         <div class="w3-container">
-          <h5 class="w3-opacity"><b>Business Information Technology</b></h5>
-          <p><b>Mahasarakham University</b>          </p>
-          <p>2019-Present</p>
-          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i>First-year</h6>
-          <p>Introduction to Business</p>
-          <p>Principles of Marketing</p>
-          <p>Business Computer and Information Technology</p>
-          <p>Introduction to Computer Programming in Business</p>
-          <p>Principles of Accounting</p>
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019</h6>
+          <p>       First year</p>
+          <p>- Information Tachnology</p>
+          <p>- Introduction to Business</p>
+          <p>- Priciples of Marketind</p>
+          <p>- Business Computer and Information Technology          </p>
           <hr>
         </div>
         <div class="w3-container">
-          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Second-year</h6>
-          <p>Business Law</p>
-          <p>Computer Technology and Operating System</p>
-          <p>Data Structure and Algorithms</p>
-          <p>Analysis and Design for Business Information System</p>
-          <p>Business Database Management System</p>
-          <p>Business Programming</p>
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2020</h6>
+          <p>       Second year</p>
+          <p>- Data Structure and Algorithms</p>
+          <p>- Computer Technology and Operating System</p>
+          <p>- Microeconomics 1</p>
+          <p>- Business Law</p>
+          <p>- Business Programming 1</p>
+          <p>- Business Database Management System</p>
+          <p>- Analysis and Design for Business Information</p>
           <hr>
         </div>
         <div class="w3-container">
-          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i><b>Third-year</b></h6>
-          <p>Production and Operations Management</p>
-          <p>Internet Marketing</p>
-          <p>Business Data Communication and Computer Network</p>
-          <p>Business Statistics</p>
-          <p>English for Business<br>
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>
+          <p>       Third year</p>
+          <p>- Business Statistics</p>
+          <p>- Production and Opeeeerations</p>
+          <p>- Taxation 1</p>
+          <p>- Internet Marketing</p>
+          <p>- Business Data Communication and Computer Network<br>
           </p>
         </div>
 
       <div class="w3-container">
-          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i><b>Four-year</b></h6>
-          <p>Business Information Technology Management</p>
-          <p>Big Data Analytics</p>
-          <p>Mobile Programming and Wireless Components in Business</p>
-          <p>Applications of Enterprise Resource Planning Software</p>
-          <p>User Experience/User Interface Design and Development</p>
-          <p>Business Policy and Strategic Management</p>
-          <p><br>
-        </p>
-      </div>
-    </div>
-      <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-arrow-up fa-fw w3-margin-right w3-xxlarge w3-text-red"></i>Self-Development</h2>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>การสร้างสรรค์เนื้อหาด้วยข้อมูล (Data) เพื่อเพิ่มคุณค่าขององค์กร </b></h5>
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Thaimooc</h6>        
-          <hr>
-        </div>
-        
-         <div class="w3-container">
-          <h5 class="w3-opacity"><b>การจัดการธุรกิจการค้าสมัยใหม่ในยุค Thailand 4.0</b></h5>
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Thaimooc</h6>      
-          <hr>
-        </div>
-        
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>เทคโนโลยีการเงิน</b></h5>
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Thaimooc</h6>       
-          <hr>
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022</h6>
+          <p>       Fourth year</p>
+          <p>- User Experience/User interface Desing and Develoment</p>
+          <p>- Applications of Enterprise Resource Planning Software</p>
+          <p>- Mobile Programming and Wireless Components in Business</p>
+          <p>- Business Information Technology Project</p>
+          <p>- Big Data Analysis<br>
+          </p>
         </div>
       </div>
-
-    <!-- End Right Column -->
-    </div>
-    
-  <!-- End Grid -->
-  </div>
+      
+      
+            </div>
+      
+     
   
   <!-- End Page Container -->
 </div>
+
+<footer class="w3-container w3-pink w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  
 
 </body>
 </html>
