e59f828a3c0a702cf2dd1c50b2c5dc0660bae412
Sukanya Mayotha <108781019+Sukanya17M@users.noreply.github.com>
2022-10-12 11:12:39 +0700

    Update indexresume.php

--- indexresume.php
+++ indexresume.php
@@ -57,5 +57,149 @@ html,body,h1 {font-family: "Cooper Black", sans-serif}
             <div class="w3-container w3-center w3-round-xlarge w3-pink" style="width:50%">75%</div>
           </div>
           <br>
+    <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-blue"></i>Languages</b></p>
+                     <p>Thai</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-pink" style="height:24px;width:90%">90%</div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-pink" style="height:24px;width:80%">60%</div>
+          </div>
+          <p>Korean</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-pink" style="height:24px;width:70%">50%</div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-orange w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Work Experience</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Student / Mahasarakham Business School, Mahasarakham University, Business Information Technology Branch (B.B.A)</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022 โครงการประกวดแผนธุรกิจ<span class="w3-tag w3-blue w3-round"></span></h6>
+        </div>
+        
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>GSB Business Case Challenge </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>เข้ารอบ 20 ทีม สุดท้าย และเป็นทีมเดียวในภาคตะวันออกเฉียงเหนื่อที่เข้ารอบ</h6>       
+        </div>
+        
+          
+      </div>
+        
+               
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-orange w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019</h6>
+          <p>       First year</p>
+          <p>- Information Tachnology</p>
+          <p>- Introduction to Business</p>
+          <p>- Priciples of Marketind</p>
+          <p>- Business Computer and Information Technology          </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2020</h6>
+          <p>       Second year</p>
+          <p>- Data Structure and Algorithms</p>
+          <p>- Computer Technology and Operating System</p>
+          <p>- Microeconomics 1</p>
+          <p>- Business Law</p>
+          <p>- Business Programming 1</p>
+          <p>- Business Database Management System</p>
+          <p>- Analysis and Design for Business Information</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>
+          <p>       Third year</p>
+          <p>- Business Statistics</p>
+          <p>- Production and Opeeeerations</p>
+          <p>- Taxation 1</p>
+          <p>- Internet Marketing</p>
+          <p>- Business Data Communication and Computer Network<br>
+          </p>
+        </div>
+
+      <div class="w3-container">
+          <h5 class="w3-opacity"><b>Mahasarskham University of Technology</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022</h6>
+          <p>       Fourth year</p>
+          <p>- User Experience/User interface Desing and Develoment</p>
+          <p>- Applications of Enterprise Resource Planning Software</p>
+          <p>- Mobile Programming and Wireless Components in Business</p>
+          <p>- Business Information Technology Project</p>
+          <p>- Big Data Analysis<br>
+          </p>
+        </div>
+      </div>
+      
+      
+            </div>
+      
+      
+    <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-pink w3-padding-16"><i class="fa fa-arrow-up fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Self-Development</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Workshop &amp; Adaptive Lecture</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022</h6>      
+          <hr>
+        </div>
+        
+      <div class="w3-container">
+          <h5 class="w3-opacity">พัฒนาชุดทดลองปัญญาประดิษฐ์เพื่อพัฒนาทักษะด้านดิจิทัล </h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022</h6>      
+          <hr>
+        </div>
+         <div class="w3-container">
+          <h5 class="w3-opacity">Digital Literacy EXIT EXAM</h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>    
+          <hr>
+      </div>
+        
+          <div class="w3-container">
+            <h5 class="w3-opacity">การสร้างสรรค์เนื้อหาข้อมูล DATA เพื่อคุณค่าขององค์กร</h5>
+            <h5 class="w3-opacity">การจัดการธุรกิจการค้าสมัยใหม่ในยุค Thailand 4.0<br>
+              เทคโนโลยีการเงิน<br>
+              สุดยอดแนวคิดธุรกิจดิจิทัล</h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>  
+          <hr>
+      </div>
+        
+    </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-pink w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="http://mbs.msu.ac.th/2022/" target="_blank">MBS,MSU</a></p>
+</footer>
+
 </body>
 </html>
+
