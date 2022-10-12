375af5da758558e4438ee5f99eeb63142daa4901
Sukanya Mayotha <108781019+Sukanya17M@users.noreply.github.com>
2022-10-12 10:30:26 +0700

    Update indexresume.php

--- indexresume.php
+++ indexresume.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+</body>
+</html>
+<!DOCTYPE html>
 <html>
 <head>
-<title>Sukanya Mayotha CV</title>
+<title>Sukanya Mayotha </title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
